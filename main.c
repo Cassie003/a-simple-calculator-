f77f00acf899f66df28093696daec75f0e92ab9c
casey minu <96833433+Cassie003@users.noreply.github.com>
2022-07-11 17:18:50 +0300

    Add files via upload
    created a simple calculator and complied online

---
+++ main.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+ 
+int main()
+{
+	char Operator;
+	float num1, num2, result = 0;
+	
+	printf("\n Please Enter an Operator (+, -, *, /)  :  ");
+  	scanf("%c", &Operator);
+  	
+	printf("\n Please Enter the Values for two Operands: num1 and num2  :  ");
+  	scanf("%f%f", &num1, &num2);
+  	
+  	switch(Operator)
+  	{
+  		case '+':
+  			printf("\n The result of %.2f + %.2f  = %.2f", num1, num2, num1 + num2);
+  			break;
+  		case '-':
+  			printf("\n The result of %.2f - %.2f  = %.2f", num1, num2, num1 - num2);
+  			break;  			
+  		case '*':
+  			printf("\n The result of %.2f * %.2f  = %.2f", num1, num2, num1 * num2);
+  			break;
+  		case '/':
+  			printf("\n The result of %.2f / %.2f  = %.2f", num1, num2, num1 / num2);
+  			break;
+		default:
+			printf("\n You have enetered an Invalid Operator ");				    			
+	}
+	
+  	return 0;
+}
